1f7eb97db479ece7355acda37773212178a31ae6
Wendy <wendy_0805@naver.com>
2024-11-22 11:54:45 +0900

    Add popspot directory

---
+++ popspot/src/test/java/kems/popspot/PopspotApplicationTests.java
@@ -0,0 +1,13 @@
+package kems.popspot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PopspotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
